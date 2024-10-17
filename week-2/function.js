c11d19f738703f55974383da817feb26375134ed
elainexu15 <elainexu15@gmail.com>
2024-10-18 01:20:11 +1300

    update

---
+++ week-2/function.js
@@ -0,0 +1,7 @@
+//function = a reusable block of code that performs a specific task. Functions can take inputs (called parameters) and can return outputs.
+
+//Why Use Functions?  Function let us reuse code
+
+function greet() {
+  console.log("Hello there");
+}
